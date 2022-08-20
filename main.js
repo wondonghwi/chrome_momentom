8f81c02cd65d35db8d2bad6ad1085faf09866108
wondonghwi <ehdehd1289@naver.com>
2022-08-20 13:08:21 +0900

    ♻️ className 명확하게 수정

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const loginForm = document.querySelector('#login-form');
-const name = document.querySelector('.name');
+const username = loginForm.querySelector('input[name="username"]');
+const title = document.querySelector('.title');
 const logout = document.querySelector('.logout');
 
 const HIDDEN_CLASSNAME = 'hidden';
@@ -8,32 +9,38 @@ const USERNAME_KEY = 'usernameKey';
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 const removeNameAndLogoutClassName = () => {
-  name.classList.remove(HIDDEN_CLASSNAME);
+  title.classList.remove(HIDDEN_CLASSNAME);
   logout.classList.remove(HIDDEN_CLASSNAME);
+};
+
+const paintingUsername = username => {
+  title.textContent = username;
 };
 
 const onLoginSubmit = e => {
   e.preventDefault();
-  const username = e.target.inputName.value;
+  const username = e.target.username.value;
 
-  name.textContent = username;
   localStorage.setItem(USERNAME_KEY, username);
+  paintingUsername(username);
 
   loginForm.classList.add(HIDDEN_CLASSNAME);
   removeNameAndLogoutClassName();
 };
 
 const onLogOut = () => {
+  username.value = '';
+
   localStorage.removeItem(USERNAME_KEY);
 
   logout.classList.add(HIDDEN_CLASSNAME);
-  name.classList.add(HIDDEN_CLASSNAME);
+  title.classList.add(HIDDEN_CLASSNAME);
   loginForm.classList.remove(HIDDEN_CLASSNAME);
 };
 
 if (savedUsername) {
   removeNameAndLogoutClassName();
-  name.textContent = savedUsername;
+  paintingUsername(savedUsername);
 } else {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
 
